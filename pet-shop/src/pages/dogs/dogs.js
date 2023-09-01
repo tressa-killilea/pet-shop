e264af6c83ee4ce7a54c57cec8a845edf3cfd045
Tressa Killilea <tressa@Tressas-MacBook-Pro.local>
2023-08-31 20:53:18 -0400

    fix bug in dogs page

--- pet-shop/src/pages/dogs/dogs.js
+++ pet-shop/src/pages/dogs/dogs.js
@@ -3,14 +3,14 @@ import Card from "../../components/Card/card";
 import Pagination from "../../components/Pagination/pagination";
 import './dogs.scss';
 
-onPageChange = (page_num) => {
-
-}
-
 export default class Dogs extends React.Component{
+    onPageChange = (page_num) => {
+
+    }
+    
     render(){
         return <div className="dogs_page">
-            <Pagination current_page={1} total_pages={4} onChange={this.onPageChange}></Pagination>
+            <Pagination current_page={1} total_pages={4} callback={this.onPageChange}></Pagination>
         </div>
     }
 }
