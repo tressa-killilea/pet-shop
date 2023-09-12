288e6b52a5b71362ccce3d35c0959d09e7208b5c
Tressa Killilea <tressa@Tressas-MacBook-Pro.local>
2023-09-12 14:04:38 -0400

    adjust grid size

--- pet-shop/src/components/Grid/grid.js
+++ pet-shop/src/components/Grid/grid.js
@@ -4,93 +4,6 @@ import "./grid.scss";
 import "../../assets/gatsby1.jpg";
 
 export default class Grid extends React.Component {
-  testData = [
-    {
-      img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-      name: "Neva",
-      breed: "Great Pyrenees",
-      age: "Puppy",
-      favorited: false,
-    },
-    {
-      img: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/59539253/1/?bust=1693372943&width=1080",
-      name: "Buffy",
-      breed: "Labrador Retriever",
-      age: "Adult",
-      favorited: false,
-    },
-    {
-      img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-      name: "Neva",
-      breed: "Great Pyrenees",
-      age: "Puppy",
-      favorited: false,
-    },
-    {
-      img: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/59539253/1/?bust=1693372943&width=1080",
-      name: "Buffy",
-      breed: "Labrador Retriever",
-      age: "Adult",
-      favorited: false,
-    },
-    {
-      img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-      name: "Neva",
-      breed: "Great Pyrenees",
-      age: "Puppy",
-      favorited: false,
-    },
-    {
-      img: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/59539253/1/?bust=1693372943&width=1080",
-      name: "Buffy",
-      breed: "Labrador Retriever",
-      age: "Adult",
-      favorited: false,
-    },
-    {
-      img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-      name: "Neva",
-      breed: "Great Pyrenees",
-      age: "Puppy",
-      favorited: false,
-    },
-    {
-      img: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/59539253/1/?bust=1693372943&width=1080",
-      name: "Buffy",
-      breed: "Labrador Retriever",
-      age: "Adult",
-      favorited: false,
-    },
-    {
-      img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-      name: "Neva",
-      breed: "Great Pyrenees",
-      age: "Puppy",
-      favorited: false,
-    },
-    {
-      img: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/59539253/1/?bust=1693372943&width=1080",
-      name: "Buffy",
-      breed: "Labrador Retriever",
-      age: "Adult",
-      favorited: false,
-    },
-    {
-      img: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-      name: "Neva",
-      breed: "Great Pyrenees",
-      age: "Puppy",
-      favorited: false,
-    },
-    {
-      img: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/59539253/1/?bust=1693372943&width=1080",
-      name: "Buffy",
-      breed: "Labrador Retriever",
-      age: "Adult",
-      favorited: false,
-    },
-  ];
-
   render() {
     console.log("GRID RENDER: "+this.props.data[0]?.name);
     const static_img = "https://media.istockphoto.com/id/1339851357/vector/dog-icon-vector-isolated-funny-puppy-head-pictogram-on-white-background.jpg?s=612x612&w=0&k=20&c=mOqd-O-dtpKjUhR52McMA8ifM-AY8H3BmJvJtgtB_ZE=";
@@ -104,7 +17,7 @@ export default class Grid extends React.Component {
       return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
     }
     return (
-      <div className="row row-cols-4 grid-wrapper">
+      <div className="row col-4-4 col-sm-6 grid-wrapper">
         {this.props.data.map(function (data, i) {
           return (
             <div className="col">
